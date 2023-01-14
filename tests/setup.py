a049261aba9a9981ed6426a2e50359cf61ef4c26
Garrett3Nelson <garrett3nelson@gmail.com>
2023-01-14 15:35:08 -0400

    Tweaking test setup for virtual testing

---
+++ tests/setup.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from sql.database import async_session, engine, Base
+from sql import models, schemas, crud
+
+
+# With a fresh database, run these two commands before pytest will work.
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def create_data():
+    item = schemas.ItemCreate(id=2, name="Cannonball", market=1000, limit=5000, members=True, high_alch=10, low_alch=5)
+    cat = schemas.CategoryCreate(name='Test', item_id=2)
+    async with async_session() as session:
+        await crud.create_item(session, item)
+        await crud.create_category(session, cat)
+
+
+if __name__ == '__main__':
+    asyncio.run(create_tables())
+    asyncio.run(create_data())
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -9,21 +9,6 @@ from sql.database import async_session, engine, Base
 from sql import models, schemas, crud
 
 
-# With a fresh database, run these two commands before pytest will work.
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-
-async def create_data():
-    item = schemas.ItemCreate(id=2, name="Cannonball", market=1000, limit=5000, members=True, high_alch=10, low_alch=5)
-    cat = schemas.CategoryCreate(name='Test', item_id=2)
-    async with async_session() as session:
-        await crud.create_item(session, item)
-        await crud.create_category(session, cat)
-
-
 @pytest_asyncio.fixture(scope="session")
 def event_loop():
     """
